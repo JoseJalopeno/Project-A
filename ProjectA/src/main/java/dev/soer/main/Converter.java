08947d8bd84aca4accd8d3c233250a6357f32e0f
Joseph Soer <soer.joseph@gmail.com>
2021-05-17 09:56:03 -0500

    Addded data validation for invalid inputs

--- ProjectA/src/main/java/dev/soer/main/Converter.java
+++ ProjectA/src/main/java/dev/soer/main/Converter.java
@@ -3,49 +3,51 @@ package dev.soer.main;
 import java.util.Scanner;
 
 public class Converter {
-
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		
 		int menuSelection = 0;
 		while(menuSelection != 4) {
+			Scanner sc = new Scanner(System.in);
 			menu();
-			menuSelection = sc.nextInt(); 
-			switch(menuSelection) {
-				case 1: {
-					System.out.println("Enter number of cups: ");
-					int cups = sc.nextInt();
-					int teaspoons = cupsToTeaspoons(cups);
-					System.out.println(cups + " cup(s) is " + teaspoons + " teaspoons");
-					System.out.println();
-					break;
+			if(sc.hasNextInt()) {
+				menuSelection = sc.nextInt();
+				switch(menuSelection) {
+					case 1: {
+						System.out.println("Enter number of cups: ");
+						double cups = sc.nextDouble();
+						double teaspoons = cupsToTeaspoons(cups);
+						System.out.println(cups + " cup(s) is " + teaspoons + " teaspoons");
+						System.out.println();
+						break;
+					}
+					case 2: {
+						System.out.println("Enter number of miles: ");
+						double miles = sc.nextDouble();
+						double kilometers = milesToKilometers(miles);
+						System.out.println(miles+ " mile(s) is " + kilometers + " kilometers");
+						System.out.println();
+						break;
+					}
+					case 3: {
+						System.out.println("Enter number of gallons(US)s: ");
+						double usGallons = sc.nextDouble();
+						double imperialGallons = usGallonsToImperial(usGallons);
+						System.out.println(usGallons + " US gallon(s) is " + imperialGallons + " imperiald gallons");
+						System.out.println();
+						break;
+					}
+					case 4: {
+						System.out.println("Thank you for using this conversion program!");
+						sc.close();
+						break;
+					}
 				}
-				case 2: {
-					System.out.println("Enter number of miles: ");
-					int miles = sc.nextInt();
-					double kilometers = milesToKilometers(miles);
-					System.out.println(miles+ " mile(s) is " + kilometers + " kilometers");
-					System.out.println();
-					break;
-				}
-				case 3: {
-					System.out.println("Enter number of gallons(US)s: ");
-					int usGallons = sc.nextInt();
-					double imperialGallons = usGallonsToImperial(usGallons);
-					System.out.println(usGallons + " US gallon(s) is " + imperialGallons + " imperiald gallons");
-					System.out.println();
-					break;
-				}
-				case 4: {
-					System.out.println("Thank you for using this conversion program!");
-					break;
-				}
-				default: {
-					System.out.println("Please enter a correct number from the menu...");
-				}
-			}	
+			}
+			else {
+				System.out.println("Please enter a correct number from the menu...");
+			}
 		}
-		sc.close();
+		
 	}
 	public static void menu() {
 		System.out.println("Please select an option:");
@@ -55,15 +57,15 @@ public class Converter {
 		System.out.println("4. Quit");
 	}
 	
-	public static int cupsToTeaspoons(int cups) {
+	public static double cupsToTeaspoons(double cups) {
 		return cups * 48;
 	}
 	
-	public static double milesToKilometers(int miles) {
+	public static double milesToKilometers(double miles) {
 		return miles * 1.60934;
 	}
 	
-	public static double usGallonsToImperial(int usGallons) {
+	public static double usGallonsToImperial(double usGallons) {
 		return usGallons * .832674;
 	}
 }
